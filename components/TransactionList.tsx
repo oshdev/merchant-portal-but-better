2cb6a996235663e4286752d07010113f7c85de88
Riya Dattani <riyaddattani@gmail.com>
2022-07-13 12:28:39 +0100

    lol minor rename

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const TrasactionList = () => {
+const TransactionList = () => {
   return <div data-testid="transaction-list"></div>;
 };
 
-export default TrasactionList;
+export default TransactionList;
